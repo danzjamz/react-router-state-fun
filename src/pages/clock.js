ce5ba55835817ab5cb4aabd3ab7337b43a8047cd
danzjamz <djl.hovley@gmail.com>
2020-03-04 11:30:16 -0700

    clock tab works

--- src/pages/clock.js
+++ src/pages/clock.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Clock() {
-    return (
-        <div className="container">
-            Clock
-        </div>
-    )
+export default class Clock extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            hour: new Date().getHours(),
+            minute: new Date().getMinutes(),
+            second: new Date().getSeconds()
+        }
+
+        console.log(this.state.second)
+    }
+
+    componentDidMount() {
+        setInterval( () => {
+            this.setState({
+                hour: new Date().getHours(),
+                minute: new Date().getMinutes(),
+                second: new Date().getSeconds()
+            });
+
+            if (this.state.second < 10) {
+                this.setState ({ second: `0${new Date().getSeconds()}` })
+            } 
+        });
+    }
+
+    render() {
+        return (
+            <div className="container" on>
+                { this.state.hour+ ':' + this.state.minute + ':' + this.state.second }
+            </div>
+        )
+    }
 }
