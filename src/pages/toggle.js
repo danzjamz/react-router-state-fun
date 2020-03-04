6ecf0fecca4632943b7a727f43481f98a96fe7cb
danzjamz <djl.hovley@gmail.com>
2020-03-04 11:37:19 -0700

    toggle works

--- src/pages/toggle.js
+++ src/pages/toggle.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Toggle() {
-    return (
-        <div className="container">
-            TOGGLE
-        </div>
-    )
+export default class Toggle extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            text: 'Hello',
+            isToggled: false
+        }
+    }
+
+    toggleText = () => {
+        this.setState({ 
+            text: (this.state.isToggled ? 'Hello' : 'Goodbye'), 
+            isToggled: !this.state.isToggled 
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {/* toggle button with hello/goodbye */}
+                
+                <button onClick={ this.toggleText }>Toggle</button>
+                <div>{ this.state.text }</div>
+    
+            </div>
+        )
+    }
 }
