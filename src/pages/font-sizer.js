7f3eed85b9c1bce87d7652e3bf34b338d031ef35
danzjamz <djl.hovley@gmail.com>
2020-03-04 12:56:43 -0700

    font-sizer works!

--- src/pages/font-sizer.js
+++ src/pages/font-sizer.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function FontSizer() {
-    return (
-        <div className="container">
-            FontSizer
-        </div>
-    )
+export default class FontSizer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            size: 12,
+            styles: {
+                'font-size': '12px'
+            }
+        }
+    }
+
+    increaseSize = () => {
+        this.setState({ size: this.state.size + 1, styles: { 'font-size':  `${ this.state.size + 1 }px` } })
+    }
+    
+    decreaseSize = () => {
+        this.setState({ size: this.state.size - 1, styles: { 'font-size':  `${ this.state.size - 1 }px` } })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div style={ this.state.styles }>
+                    { this.state.styles['font-size'] }
+                    <button onClick={ this.increaseSize }>+</button>
+                    <button onClick={ this.decreaseSize }>-</button>
+                </div>
+            </div>
+        )
+    }
 }
